74ee20ea39864904296acf0e4331fd9f933779b4
BicheRusse <comboice69@gmail.com>
2024-11-20 19:40:56 +0100

    BO/DAO
    Update BO

--- src/Model/BO/Bilan1.php
+++ src/Model/BO/Bilan1.php
@@ -1,6 +1,19 @@
 <?php
 
-class Bilan1
-{
+require_once 'Bilan.php';
 
+class Bilan1 extends Bilan {
+    public function __construct(
+        int $idBil,
+        string $notorBil,
+        string $moyBil,
+        string $remarqueBil,
+        string $sujMenBil,
+        DateTime $datBil,
+        DateTime $datLimBil
+    ) {
+        parent::__construct($idBil, $notorBil, $moyBil, $remarqueBil, $sujMenBil, $datBil, $datLimBil);
+    }
+
+    // Ajoute des méthodes spécifiques à Bilan1 si nécessaire.
 }
