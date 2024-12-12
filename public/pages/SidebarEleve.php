420a4057fa3e6f3094db4f748b4aebc42545dbc0
Croissantdett <eloipoly63@gmail.com>
2024-12-12 14:09:00 +0100

    Modification sidebar eleve

--- public/pages/SidebarEleve.php
+++ public/pages/SidebarEleve.php
@@ -1,12 +1,9 @@
 <nav class="sidebar">
     <div class="logo">
-        <img src="../../src/img/FSI_logo.png" alt="Logo">
+        <img src="../img/FSI_logo.png" alt="Logo">
     </div>
-    <ul class="nav-linksfir">
-        <li class="active"><a >Tableau de bord</a></li>
-        <li class="active"><a >Mes informations</a></li>
+    <ul class="nav-links">
+        <li><a href="#">Tableau de board</a></li>
+        <li><a href="#">Mes informations</a></li>
     </ul>
-    <div class="collapse-button">
-        <span class="collapse-icon"><i class="fa-solid fa-chevron-left"></i></span>
-    </div>
 </nav>
